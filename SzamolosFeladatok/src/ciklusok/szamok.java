1845c2a7400142efca3c038c3b1662bb38d88b03
Erno1234 <palocierno12>
2023-03-03 12:34:16 +0100

    2 oszlop

---
+++ SzamolosFeladatok/src/ciklusok/szamok.java
@@ -0,0 +1,16 @@
+
+package ciklusok;
+
+
+public class szamok {
+      public static void main(String[] args) {
+          for (int j = 2; j <= 3; j++) {
+            for (int i = 2; i < 5; i++) {
+              System.out.println(j+" "+i);
+              
+          }  
+          }
+          
+    }
+    
+}
